cf9d1f33e9d0f1046e771cb35d8afd7a3a76384c
chinecheremdivine797-spec <chinecheremdivine797@gmail.com>
2026-09-11 17:57:15 +0100

    Add DIV TV Supabase config

---
+++ config.js
@@ -0,0 +1,6 @@
+window.DIV_TV_CONFIG = {
+  supabaseUrl: 'https://iyoxcubcxgywybkufjab.supabase.co',
+  supabaseKey: 'sb_publishable_lOk4ZiVjiV4inQhndmxxJw_-zAnmyGq',
+  adminEmail: 'divstudio03@gmail.com',
+  storageBucket: 'divstudio-videos'
+};
